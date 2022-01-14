35619ed9d91307abdab0e08379fa9c9403a1f6df
maddocnc <maddocnc@gmail.com>
2022-01-14 13:20:44 +0300

    upgrade deps

--- index.js
+++ index.js
@@ -3,11 +3,6 @@
 const names = '^[_-]?[a-z][a-z0-9-]+?[a-z0-9]$'
 
 module.exports = {
-  extends: [
-    'stylelint-config-standard',
-    'stylelint-config-recommended-scss',
-  ],
-
   plugins: [
     'stylelint-order',
     'stylelint-declaration-strict-value',
@@ -21,10 +16,14 @@ module.exports = {
     'stylelint-csstree-validator',
   ],
 
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-recommended-scss',
+    'stylelint-config-prettier'
+  ],
+
   rules: {
     'max-nesting-depth': 4,
-    'max-line-length': 80,
-    'string-quotes': 'single',
     'font-weight-notation': 'numeric',
     'order/order': [
       [
@@ -37,27 +36,27 @@ module.exports = {
         'dollar-variables',
         {
           'type': 'at-rule',
-          'name': 'extend',
-        },
-        {
-          'type': 'at-rule',
           'name': 'include',
           'hasBlock': false,
         },
+        {
+          'type': 'at-rule',
+          'name': 'extend',
+        },
         'declarations',
+        'rules',
+        'at-rules',
         {
           'type': 'at-rule',
           'name': 'include',
           'hasBlock': true,
         },
-        'rules',
-        'at-rules',
       ],
     ],
     'function-url-no-scheme-relative': true,
     'function-url-quotes': 'always',
     'media-feature-name-no-unknown': true,
-    'media-query-list-comma-newline-before': 'never-multi-line',
+
     'scss/media-feature-value-dollar-variable': 'always',
 
     'at-rule-no-vendor-prefix': true,
@@ -66,7 +65,7 @@ module.exports = {
     'value-no-vendor-prefix': true,
     'selector-no-vendor-prefix': true,
 
-    // 'scss/double-slash-comment-whitespace-inside': 'always',
+    'scss/double-slash-comment-whitespace-inside': 'always',
     'scss/declaration-nested-properties': 'never',
     'scale-unlimited/declaration-strict-value': [
       ['/color/', 'z-index', 'font-size', 'font-family'],
@@ -94,46 +93,21 @@ module.exports = {
     'scss/percent-placeholder-pattern': names,
     'scss/at-extend-no-missing-placeholder': true,
     // 'scss/at-mixin-named-arguments': 'always',
-    'scss/at-mixin-parentheses-space-before': 'never',
-    'scss/at-mixin-argumentless-call-parentheses': 'never',
     'scss/at-mixin-pattern': names,
     // 'scss/at-function-named-arguments': 'always',
-    'scss/at-function-parentheses-space-before': 'never',
     'scss/at-function-pattern': names,
-    'scss/operator-no-newline-before': true,
-    'scss/operator-no-newline-after': true,
-    'scss/operator-no-unspaced': true,
-    'selector-attribute-quotes': 'always',
+
     'selector-max-universal': 1,
     'selector-max-specificity': '1,3,3', // id,class,type
     'selector-max-compound-selectors': 3,
     'scss/selector-no-redundant-nesting-selector': true,
     'scss/at-import-no-partial-leading-underscore': true,
     'scss/at-import-partial-extension-blacklist': ['scss'],
-    'at-rule-empty-line-before': [
-      'always', {
-        'ignoreAtRules': ['else'],
-        'except': [
-          'blockless-after-same-name-blockless',
-          'first-nested',
-        ],
-        'ignore': ['after-comment'],
-      },
-    ],
-    'block-closing-brace-newline-after': [
-      'always', { 'ignoreAtRules': ['if', 'else'] },
-    ],
 
-    'scss/at-else-closing-brace-newline-after': 'always-last-in-chain',
-    'scss/at-else-closing-brace-space-after': 'always-intermediate',
-    'scss/at-else-empty-line-before': 'never',
-    'scss/at-else-if-parentheses-space-before': 'always',
-    'scss/at-if-closing-brace-newline-after': 'always-last-in-chain',
-    'scss/at-if-closing-brace-space-after': 'always-intermediate',
     'color-named': 'never',
     // 'color-format/format': { 'format': 'rgb' },
 
-    'plugin/no-low-performance-animation-properties': true,
+    'plugin/no-low-performance-animation-properties': [true, { ignoreProperties: ['color', 'background-color'] }],
     'plugin/stylelint-no-indistinguishable-colors': true,
     // 'plugin/selector-tag-no-without-class': ['div', 'span'],
     'plugin/declaration-block-no-ignored-properties': true,
